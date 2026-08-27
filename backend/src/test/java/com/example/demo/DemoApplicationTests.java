52b72ab47ee76a7574f8dfebe1b075a342b5b635
Achraf <achraf.ouazzane00@gmail.com>
2026-08-27 14:41:17 +0200

    last version of backend

--- backend/src/test/java/com/example/demo/DemoApplicationTests.java
+++ backend/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,9 +1,17 @@
-package com.example.demo;
+package com.rrm.parking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.datasource.url=jdbc:h2:mem:rrm_parking;MODE=MySQL;DB_CLOSE_DELAY=-1;DATABASE_TO_LOWER=TRUE;INIT=CREATE SCHEMA IF NOT EXISTS rrm_parking",
+		"spring.datasource.driver-class-name=org.h2.Driver",
+		"spring.datasource.username=sa",
+		"spring.datasource.password=",
+		"spring.jpa.hibernate.ddl-auto=create-drop",
+		"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+		"spring.flyway.enabled=false"
+})
 class DemoApplicationTests {
 
 	@Test
